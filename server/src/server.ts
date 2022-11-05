1ec0bbb81871cc465be185fd1f50a5a27a9e06fc
lucas-mateus <xlucasmat@gmail.com>
2022-11-05 12:09:08 -0300

    Criando rotas da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import {PrismaClient} from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import {z} from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
-const primsa = new PrismaClient({
+const prisma = new PrismaClient({
     log: ['query'],
 })
 
@@ -16,9 +18,46 @@ async function bootstrap(){
         origin: true
     })
 
+
     fastify.get('/pools/count', async () =>{
-        const count = await primsa.pool.count()
+        const count = await prisma.pool.count()
         return { count }
+    })
+
+    fastify.get('/users/count', async () =>{
+        const count = await prisma.user.count()
+        return { count }
+    })
+
+    fastify.get('/guesses/count', async () =>{
+        const count = await prisma.guess.count()
+        return { count }
+    })
+
+    
+    fastify.post('/pools', async (request, reply) =>{
+        
+        const createPoolBody = z.object({
+            title: z.string(),
+
+        })
+
+        const {title} = createPoolBody.parse(request.body)
+        
+        const generate = new ShortUniqueId({length:6})
+        const code = String(generate()).toUpperCase()
+
+        await prisma.pool.create({
+            data:{
+                code,
+                title,
+            }
+        })
+
+        return reply.status(201).send({code})
+        
+
+
     })
 
     await fastify.listen({port: 3333})
