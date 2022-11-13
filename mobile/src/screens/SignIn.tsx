1d38e9f64b86e9c293ac8f4201a225e161154817
lucas-mateus <xlucasmat@gmail.com>
2022-11-13 12:26:21 -0300

    Criando button component & SignIn interface

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,12 +1,23 @@
-import { Center, Text} from 'native-base'
+import { Center, Text, Icon} from 'native-base'
+import Logo from '../assets/logo.svg'
+import {Fontisto} from '@expo/vector-icons'
+import { Button } from '../components/Button'
 
 export function SignIn (){
 
     return(
-        <Center flex={1} bgColor="gray.900">
-            <Text color="white" fontSize={24}>
-                Sign In Tela
+        <Center flex={1} bgColor="gray.900" p={6}>
+            <Logo width={212} height={40}/>
+
+            <Button mt={12} mb={4}
+                type={"SECONDARY"}
+                title="ENTRAR COM O GOOGLE"
+                leftIcon={<Icon as={Fontisto} color={'white'} name={"google"} size={"md"} />}
+            />
+
+            <Text color="white">
+                Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
             </Text>
-      </Center>
+        </Center>
     )
 }
