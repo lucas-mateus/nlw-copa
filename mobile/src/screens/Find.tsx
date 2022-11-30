0bfc8454c08900769c8a25c4b3fdf588981c741a
lucas-mateus <xlucasmat@gmail.com>
2022-11-30 19:07:55 -0300

    Criando tela de busca de bolão

---
+++ mobile/src/screens/Find.tsx
@@ -0,0 +1,23 @@
+import { Heading, VStack } from "native-base";
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+
+export function Find() {
+  return (
+    <VStack flex={1} bgColor="gray.900" >
+        <Header title={"Buscar por código"} showBackButton />
+        
+        <VStack mt={8} mx={8} alignItems={"center"}>
+
+            <Heading textAlign={'center'} color={'white'} fontSize={'xl'} fontFamily={'heading'}>
+                Encontre um bolão através de seu código único   
+            </Heading>
+
+            <Input mt={8} placeholder={"Qual o código do bolão?"}/>
+            <Button mt={2} title={'Buscar bolão'} />
+
+        </VStack>
+    </VStack>
+  )
+}
